5f74b21c065cdd241c1d00843f0bc1df8cf2004c
ziyuan <xzy_study@163.com>
2017-10-06 08:57:26 +0800

    part vi. code has been updated, but never login use openID, and don't know how.

--- app/models.py
+++ app/models.py
@@ -1,11 +1,13 @@
 from app import db
+from hashlib import md5
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     nickname = db.Column(db.String(64), index = True, unique = True)
     email = db.Column(db.String(120), index = True, unique = True)
     posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
-
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime)
     def is_authenticated(self):
         return True
 
@@ -24,8 +26,12 @@ class User(db.Model):
     def __repr__(self):
         return '<User %r>' % (self.nickname)
 
+    def avatar(self, size):
+        return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' % (md5(self.email.encode('utf-8')).hexdigest(), size)
+        # return ''
+
 class Post(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
--- app/views.py
+++ app/views.py
@@ -1,18 +1,41 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
-from .forms import LoginForm
+from .forms import LoginForm,  EditForm
 from .models import User
+from datetime import datetime
+
+
+@app.route('/edit', methods=['GET', 'POST'])
+@login_required
+def edit():
+    form = EditForm()
+    if form.validate_on_submit():
+        g.user.nickname = form.nickname.data
+        g.user.about_me = form.about_me.data
+        db.session.add(g.user)
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit'))
+    else:
+        form.nickname.data = g.user.nickname
+        form.about_me.data = g.user.about_me
+    return render_template('edit.html', form=form)
 
 @app.before_request
 def before_request():
     g.user = current_user
+    if g.user.is_authenticated:
+        g.user.last_seen = datetime.utcnow()
+        db.session.add(g.user)
+        db.session.commit()
+
 
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
-    # user = { 'nickname': 'Miguel' } # fake user
+    #user = { 'nickname': 'Miguel' } # fake user
     user = g.user
     posts = [ # fake array of posts
         {
@@ -71,3 +94,18 @@ def after_login(resp):
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/user/<nickname>')
+# @login_required
+def user(nickname):
+    user = User.query.filter_by(nickname = nickname).first()
+    if user == None:
+        flash('User ' + nickname + ' not found.')
+        return redirect(url_for('index'))
+    posts = [
+        { 'author': user, 'body': 'Test post #1' },
+        { 'author': user, 'body': 'Test post #2' }
+    ]
+    return render_template('user.html',
+        user = user,
+        posts = posts)
